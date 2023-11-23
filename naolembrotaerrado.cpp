f8e96115981e6c175d87f9428bb1eebb519069e3
Zago! <144025196+zagozagoo@users.noreply.github.com>
2023-11-23 14:14:02 -0300

    Update naolembrotaerrado.cpp

--- naolembrotaerrado.cpp
+++ naolembrotaerrado.cpp
@@ -22,6 +22,8 @@ void loop()
   float humidade = dht.readHumidity();
   float temperatura = dht.readTemperature();
 
+  Serial.printf("Humidade: %4.2f \nTemperatura: %7.2f\n\n", humidade, temperatura); //o 7 da o valor total da string pra ficar alinhada
+
   if (isnan(humidade) || isnan(temperatura))
   {
     Serial.println("Falha na leitura do sensor DHT");
@@ -29,4 +31,4 @@ void loop()
   }
 }   
 
-Serial.printf("Humidade: %4.2f \nTemperatura: %7.2f\n\n", humidade, temperatura); //o 7 da o valor total da string pra ficar alinhada
+
