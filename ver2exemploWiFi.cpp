1f06163c9b76eef9bf6bbd77369d6ba533721a46
Zago! <144025196+zagozagoo@users.noreply.github.com>
2023-11-27 15:28:07 -0300

    Update ver2exemploWiFi.cpp

--- ver2exemploWiFi.cpp
+++ ver2exemploWiFi.cpp
@@ -10,32 +10,25 @@ void setup()
 
     WiFi.mode(WIFI_STA);
     WiFi.disconnect(); //garante que a esp nao ta conectada em outro wifi
-
-    WiFi.begin("ssid", "senhas");
-}
-
-void loop()
-{
+    
     int qtde_wifi = WiFi.scanNetworks();
 
     if(qtde_wifi == 0) //caso nao tenha nenhum wifi ele nao faz nada
     {
             return;
     }
-    for (int i = 0; i i < qtde_wifi; i++)
-    {
-        Serial.printf("Quantidade de redes disponiveis:  %i \n", qtde_wifi);
-        Serial.print("Indice: ");
-        Serial.print(i + 1);
-        Serial.print(" SSID: ");
-        Serial.print(WiFi.SSID(i));  //Nome da rede
-        Serial.print(" RSSI: ");
-        Serial.print(WiFi.RSSI(i)); //intensidade do sinal
-        Serial.print(" Auth: ");
-        Serial.print(WiFi.encryptionType(i));
-        Serial.println();
+   
+    WiFi.begin(WIFI_SSID, WIFI_PW);
 
-    }
+    Serial.print("Conectando")
+    while(WiFi.status() != WL_CONNECTED) //enquanto nao estiver conectado ele continua tentando
+        {
+            Serial.print(".");
+            delay(300);
+        }
+}
 
+void loop()
+{
     delay(5000);
 }
