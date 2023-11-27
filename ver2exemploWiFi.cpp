820557401048ac94ff161754305225fc1bb1b1f1
Zago! <144025196+zagozagoo@users.noreply.github.com>
2023-11-27 15:36:07 -0300

    Update ver2exemploWiFi.cpp

--- ver2exemploWiFi.cpp
+++ ver2exemploWiFi.cpp
@@ -4,10 +4,8 @@
 const char* WIFI_SSID = "nomedarede";
 const char* WIFI_PW = "senha";
 
-void setup()
+bool connectWiFi(const char* ssid, const char* pw)
 {
-    Serial.begin(115200);
-
     WiFi.mode(WIFI_STA);
     WiFi.disconnect(); //garante que a esp nao ta conectada em outro wifi
     
@@ -20,12 +18,30 @@ void setup()
    
     WiFi.begin(WIFI_SSID, WIFI_PW);
 
-    Serial.print("Conectando")
+    Serial.print("Conectando");
+    int tentativas = 0;
     while(WiFi.status() != WL_CONNECTED) //enquanto nao estiver conectado ele continua tentando
         {
+            tentativa++;
+
+            if (tentativa > 300)
+            {
+                return false;
+            }
+            
             Serial.print(".");
             delay(300);
         }
+    
+    Serial.println(Conectando com o IP: );
+
+    sERIAL.PRINTLN(WiFi.localIP());
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    connectWiFi(WIFI_SSID, WIFI_PW);
 }
 
 void loop()
