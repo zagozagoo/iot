c24c44de4a5ca07a4d1816b6de1633ae2887b324
Zago! <144025196+zagozagoo@users.noreply.github.com>
2023-11-27 15:24:01 -0300

    Create ver2exemploWiFi.cpp

---
+++ ver2exemploWiFi.cpp
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+#include <Arduino.h>
+
+const char* WIFI_SSID = "nomedarede";
+const char* WIFI_PW = "senha";
+
+void setup()
+{
+    Serial.begin(115200);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect(); //garante que a esp nao ta conectada em outro wifi
+
+    WiFi.begin("ssid", "senhas");
+}
+
+void loop()
+{
+    int qtde_wifi = WiFi.scanNetworks();
+
+    if(qtde_wifi == 0) //caso nao tenha nenhum wifi ele nao faz nada
+    {
+            return;
+    }
+    for (int i = 0; i i < qtde_wifi; i++)
+    {
+        Serial.printf("Quantidade de redes disponiveis:  %i \n", qtde_wifi);
+        Serial.print("Indice: ");
+        Serial.print(i + 1);
+        Serial.print(" SSID: ");
+        Serial.print(WiFi.SSID(i));  //Nome da rede
+        Serial.print(" RSSI: ");
+        Serial.print(WiFi.RSSI(i)); //intensidade do sinal
+        Serial.print(" Auth: ");
+        Serial.print(WiFi.encryptionType(i));
+        Serial.println();
+
+    }
+
+    delay(5000);
+}
